0271b07734d142404e5cfd15b4e323e868915c91
rohanvron <164373071+rohanvron@users.noreply.github.com>
2024-08-25 01:21:19 +0530

    config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/DataViz/',
   plugins: [react()],
   server: {
     proxy: {
-      base: '/DataViz/',
       '/api': 'https://data-viz-three.vercel.app'
     }
   }
